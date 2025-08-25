8ec00ff07e1f055f7ff390fd02eea2b03e3eb4f7
BrahmanNitish <120305293+BrahmanNitish@users.noreply.github.com>
2025-08-25 23:06:53 +0530

    Add files via upload

---
+++ assign5t2.py
@@ -0,0 +1,6 @@
+my_list=[1,2,3,4,5,6,7,8,9,10]
+print("Original List:", my_list)
+first_five=my_list[:5]
+print("Extracted First five elements:", first_five)
+reversed_extracted_list=first_five[::-1]
+print("Reversed Extracted Elements:", reversed_extracted_list)
